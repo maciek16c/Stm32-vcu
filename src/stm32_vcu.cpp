61fde5758f0402a1a2e647b56fb170eafdba2e38
maciek16c <maciekbarcikowski@gmail.com>
2025-10-25 14:40:31 +0200

    prechargeMinTime fix

--- src/stm32_vcu.cpp
+++ src/stm32_vcu.cpp
@@ -791,8 +791,9 @@ static void Ms10Task(void) {
       DigIo::prec_out.Set(); // commence precharge
     if (prechargeMinTime != 0)
       prechargeMinTime--; // 1 second minimum precharge time
-    if ((prechargeMinTime == 0 && stt & (STAT_POTPRESSED | STAT_UDCBELOWUDCSW |
-                                         STAT_UDCLIM)) == STAT_NONE) {
+    if ((prechargeMinTime == 0) &&
+        ((stt & (STAT_POTPRESSED | STAT_UDCBELOWUDCSW | STAT_UDCLIM)) ==
+         STAT_NONE)) {
       if (StartSig) {
         opmode = MOD_RUN;
         StartSig = false;                    // reset for next time
